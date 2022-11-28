782042514ecd5773552d20ef5f1f644e1e037baf
Manuel <juanzatch17@hotmail.com>
2022-11-28 17:36:35 -0500

    adding todolist project

---
+++ date.js
@@ -0,0 +1,27 @@
+//make a function avaliable in other parts | creates a module
+
+exports.getDate = function() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
+
+exports.getDay = function() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
